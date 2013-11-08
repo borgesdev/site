c7f9a4ec88763989fb2502b845f2d5490dc85cce
Magno Dal Magro <Magno Dal Magro@Magno-PC>
2013-11-08 20:17:54 -0200

    Chamando estilo.css no head do site

--- wp-content/themes/borges/contato.php
+++ wp-content/themes/borges/contato.php
@@ -14,18 +14,20 @@ the_post();
 
     <div class="float-left contato-esquerda">
          
+      <div>         
+        <p class="mapa-linha" width="93%" heigh="0px"></p>
+      </div>
       <div class="mapa">         
-        <iframe width="93%" height="98%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt-BR&amp;geocode=&amp;q=Rua+das+Laranjeiras,+260,+Rio+de+Janeiro&amp;aq=&amp;sll=-22.934296,-43.18732&amp;sspn=0.008418,0.013926&amp;t=m&amp;ie=UTF8&amp;hq=&amp;hnear=R.+das+Laranjeiras,+260+-+Laranjeiras,+Rio+de+Janeiro,+22240-003&amp;ll=-22.934296,-43.18732&amp;spn=0.008418,0.013926&amp;z=14&amp;output=embed"></iframe>
+        <iframe width="100%" height="98%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  src="https://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt-BR&amp;geocode=&amp;q=Rua+das+Laranjeiras,+260,+Rio+de+Janeiro&amp;aq=&amp;sll=-22.934296,-43.18732&amp;sspn=0.008418,0.013926&amp;t=m&amp;ie=UTF8&amp;hq=&amp;hnear=R.+das+Laranjeiras,+260+-+Laranjeiras,+Rio+de+Janeiro,+22240-003&amp;ll=-22.934296,-43.18732&amp;spn=0.008418,0.013926&amp;z=14&amp;output=embed"></iframe>
       </div>
       
-       
+      
       <div class="contato-horarios">
+        <p class="dados">Horário de funcionamento</p>
         <? the_content() ?>
       </div>
     
     </div>
-    
-   
     
   <div class='titulo'>
     <span class="linha-titulo-contato"> </span>
@@ -67,18 +69,18 @@ the_post();
         </div>      
 
         <div>      
-          <input type="text" name="nome" id="nome" value="NOME*" onfocus="if(this.value==='NOME*')this.value='';" onblur="if (this.value==='')this.value='NOME*';"/>
+          <input type="text" name="nome" id="nome" value="Nome*" onfocus="if(this.value==='Nome*')this.value='';" onblur="if (this.value==='')this.value='NOME*';"/>
         </div>
 
         <div>
-          <input type="text" name="email" id="email" value="E-MAIL*" onfocus="if(this.value==='E-MAIL*')this.value='';" onblur="if (this.value==='')this.value='E-MAIL*';"/>
+          <input type="text" name="Email" id="email" value="Email*" onfocus="if(this.value==='email*')this.value='';" onblur="if (this.value==='')this.value='E-MAIL*';"/>
         </div>
         <div>
-          <input type="text" class="ddd" name="ddd" id="ddd" value="DDD*" onfocus="if(this.value==='DDD*')this.value='';" onblur="if (this.value==='')this.value='DDD*';"/>
-          <input type="text" class="telefone "name="telefone" id="telefone" value="TELEFONE*" onfocus="if(this.value==='TELEFONE*')this.value='';" onblur="if (this.value==='')this.value='TELEFONE*';"/>
+          <input type="text" class="ddd" name="ddd" id="ddd" value="ddd*" onfocus="if(this.value==='ddd*')this.value='';" onblur="if (this.value==='')this.value='DDD*';"/>
+          <input type="text" class="telefone "name="telefone" id="telefone" value="Telefone*" onfocus="if(this.value==='Telefone*')this.value='';" onblur="if (this.value==='')this.value='TELEFONE*';"/>
         </div>
         <div>
-          <textarea name="mensagem" id="mensagem" class="mensagem" onfocus="if(this.value==='MENSAGEM')this.value='';" onblur="if (this.value==='')this.value='MENSAGEM';">MENSAGEM</textarea>
+          <textarea name="mensagem" id="mensagem" class="mensagem" onfocus="if(this.value==='Mensagem')this.value='';" onblur="if (this.value==='')this.value='Mensagem';">Mensagem</textarea>
         </div>
         <div>
           <input type="submit" value="Enviar" id="submit"/>
