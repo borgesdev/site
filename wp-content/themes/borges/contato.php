a04cbaa7856df54e17f389dc60eb40b28b3903de
joao <joao@192.168.0.130>
2013-11-09 14:24:08 -0200

    Integrando contatos

--- wp-content/themes/borges/contato.php
+++ wp-content/themes/borges/contato.php
@@ -13,43 +13,56 @@ the_post();
   <div class="contato-bloco">
 
     <div class="float-left contato-esquerda">
-        
-      <div>         
-        <p class="mapa-linha" width="93%" heigh="0px"></p>
+      <div>
+        <div class="mapa position-relative">         
+          <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  src="https://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt-BR&amp;geocode=&amp;q=Rua+das+Laranjeiras,+260,+Rio+de+Janeiro&amp;aq=&amp;sll=-22.934296,-43.18732&amp;sspn=0.008418,0.013926&amp;t=m&amp;ie=UTF8&amp;hq=&amp;hnear=R.+das+Laranjeiras,+260+-+Laranjeiras,+Rio+de+Janeiro,+22240-003&amp;ll=-22.934296,-43.18732&amp;spn=0.008418,0.013926&amp;z=14&amp;output=embed"></iframe>
+          <div class="corte-inferior-direito"></div>
+          <div class="tarja-img azul-borges-bg"></div>
+        </div>
+
       </div>
-      
-      <div class="mapa">         
-        <iframe width="100%" height="98%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  src="https://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt-BR&amp;geocode=&amp;q=Rua+das+Laranjeiras,+260,+Rio+de+Janeiro&amp;aq=&amp;sll=-22.934296,-43.18732&amp;sspn=0.008418,0.013926&amp;t=m&amp;ie=UTF8&amp;hq=&amp;hnear=R.+das+Laranjeiras,+260+-+Laranjeiras,+Rio+de+Janeiro,+22240-003&amp;ll=-22.934296,-43.18732&amp;spn=0.008418,0.013926&amp;z=14&amp;output=embed"></iframe>
-      </div>
-      
-      
+
       <div class="contato-horarios">
         <p class="dados">Horário de funcionamento</p>
         <? the_content() ?>
       </div>
-    
+
     </div>
-    
-  <div class='titulo'>
-    <span class="linha-titulo-contato"> </span>
-    <h1 class="azul-borges">Contato</h1>
-  </div>
-    
+
+    <div class='titulo'>
+      <span class="linha-titulo-contato"></span>
+      <h1 class="azul-borges">Contato</h1>
+    </div>
+
     <div class="contato-endereco">
       <p class="dados">Email</p>
-        <? the_content() ?>
+      <? $camposExtras = get_post_custom();
+      echo $camposExtras['Email'][0];
+      ?> 
       <p class="dados2">Unidades</p>
-        <? the_content() ?>
-      <p class="dados-3"><? the_content() ?></p>
-      <p><a href="#"><< Localizar no mapa</a>
-        <? the_content() ?>
-      <p class="dados-3"><? the_content() ?></p>
-        <? the_content() ?>
-      <p><a href="#"><< Localizar no mapa</a>
+
+      <?
+      global $wp_query;
+      $wp_query = new WP_Query(array('cat' => _UNIDADES));
+      //var_dump($wp_query->posts);
+      while (have_posts()) {
+        the_post();
+        ?>
+        <h3 class="marrom-borges"><? the_title() ?></h3>
+        <p class="dados-3"><? the_content() ?></p>
+        <h4 class="azul-borges">Telefones</h4>
+        <div class="telefones">
+          <?= str_replace('...', '', get_the_excerpt()) ?>
+        </div>
+        <p><a href="#"><< Localizar no mapa</a>
+          <?
+        }
+        ?>       
+
     </div>
-    
+
     <div class="float-right contato-direita"> 
-      
+
       <form action="" method="post"  enctype="multipart/form-data" id="form-contato">
         <div>
           <div class="styled-select">
@@ -59,7 +72,6 @@ the_post();
               <option value="Elogios">Elogios</option>
               <option value="Críticas">Críticas</option>
               <option value="Depoimentos">Depoimentos</option>
-              <option value="Envio de Currículo">Envio de Currículo</option>
             </select>
           </div>
         </div>
@@ -70,25 +82,35 @@ the_post();
         </div>      
 
         <div>      
-          <input type="text" name="nome" id="nome" value="Nome*" onfocus="if(this.value==='Nome*')this.value='';" onblur="if (this.value==='')this.value='NOME*';"/>
+          <input type="text" name="nome" id="nome" value="Nome*" onfocus="if (this.value === 'Nome*')
+                this.value = '';" onblur="if (this.value === '')
+                this.value = 'NOME*';"/>
         </div>
 
         <div>
-          <input type="text" name="Email" id="email" value="Email*" onfocus="if(this.value==='email*')this.value='';" onblur="if (this.value==='')this.value='E-MAIL*';"/>
+          <input type="text" name="Email" id="email" value="Email*" onfocus="if (this.value === 'email*')
+                this.value = '';" onblur="if (this.value === '')
+                this.value = 'E-MAIL*';"/>
         </div>
         <div>
-          <input type="text" class="ddd" name="ddd" id="ddd" value="ddd*" onfocus="if(this.value==='ddd*')this.value='';" onblur="if (this.value==='')this.value='DDD*';"/>
-          <input type="text" class="telefone "name="telefone" id="telefone" value="Telefone*" onfocus="if(this.value==='Telefone*')this.value='';" onblur="if (this.value==='')this.value='TELEFONE*';"/>
+          <input type="text" class="ddd" name="ddd" id="ddd" value="ddd*" onfocus="if (this.value === 'ddd*')
+                this.value = '';" onblur="if (this.value === '')
+                this.value = 'DDD*';"/>
+          <input type="text" class="telefone "name="telefone" id="telefone" value="Telefone*" onfocus="if (this.value === 'Telefone*')
+                this.value = '';" onblur="if (this.value === '')
+                this.value = 'TELEFONE*';"/>
         </div>
         <div>
-          <textarea name="mensagem" id="mensagem" class="mensagem" onfocus="if(this.value==='Mensagem')this.value='';" onblur="if (this.value==='')this.value='Mensagem';">Mensagem</textarea>
+          <textarea name="mensagem" id="mensagem" class="mensagem" onfocus="if (this.value === 'Mensagem')
+                this.value = '';" onblur="if (this.value === '')
+                this.value = 'Mensagem';">Mensagem</textarea>
         </div>
         <div>
           <input type="submit" value="Enviar" id="submit"/>
         </div>
       </form>
     </div>
-    
+
     <div class="clear"></div>
   </div>
 </div>
